ad04588c09a863d659253964f71b887c78ff5491
TincoNomad <rtincopa@proton.me>
2024-01-05 13:58:27 -0500

    🎨style: fix logic to ger rpcURL from payment service. remove no use code from create tokend & create NFT

--- src/modules/payment/controllers/payment.controller.ts
+++ src/modules/payment/controllers/payment.controller.ts
@@ -6,22 +6,33 @@ import {
   UsePipes, 
   ValidationPipe 
 } from '@nestjs/common';
+import { WalletService } from '../../wallet/services/wallet.service';
 import { PaymentService } from '../services/payment.service';
 import { SendPaymentDto } from '../dto/send-payment.dto';
 
 @Controller('payment')
 export class PaymentController {
-  constructor(private readonly paymentService: PaymentService) { }
-
+  constructor(
+    private readonly paymentService: PaymentService,
+    private readonly walletService: WalletService,
+    ) { }
+  
+   /**
+   * @memberof PaymentController
+   * @description Send an ERC721 Token.
+   * @param {Object} sendPayment - Payment Parameters.
+   * @returns {string} - Receiver Address.
+   */  
   @Post('send')
   @UsePipes(ValidationPipe)
   async sendTokens(
     @Body() sendPayment: SendPaymentDto,
     @Query('chain') chain: string,
   ) {
+    const rpcUrl = await this.walletService.getRpcUrl(chain);
     return await this.paymentService.sendERC20tokens(
-      chain,
-      sendPayment
+      sendPayment,
+      rpcUrl,
     );
   }
 }
