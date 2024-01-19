37393a92e39dea8703408c07d361f231595a3ae7
TincoNomad <rtincopa@proton.me>
2024-01-18 19:31:13 -0500

    🗃️DB: add User Crud

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,53 +1,49 @@
 import {
-  Controller,
   Get,
-  Post,
   Body,
   Patch,
   Param,
-  Delete,
   Logger,
+  Delete,
+  Headers,
+  Controller,
   ParseIntPipe,
-  UseGuards,
 } from '@nestjs/common';
 import { UserService } from '../services/user.service';
-import { CreateUserDto } from '../dto/create-user.dto';
-import { UpdateUserDto } from '../dto/update-user.dto';
+import { passwordDto } from '../dto/password.dto';
 import { GetUserDto } from '../dto';
 import { User } from '../entities/user.entity';
-import { AuthGuard } from '@nestjs/passport';
-import { RoleProtect } from '../../role/decorators/role.decorator';
-import { RoleTypeEnum } from '../../../shared/enums/role-type.enum';
-import { RoleGuard } from '../../role/guards/role.guard';
 
 @Controller('user')
 export class UserController {
   private readonly _logger = new Logger(':::: UserController ::::');
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto): Promise<GetUserDto> {
-    this._logger.log(JSON.stringify(createUserDto));
-    return this.userService.create(createUserDto);
+  @Get()
+  async findOne(
+    @Headers('authorization') authHeader: string,
+  ): Promise<GetUserDto> {
+    return await this.userService.get(authHeader);
   }
 
-  @Get(':id')
-  @RoleProtect(RoleTypeEnum.ADMIN, RoleTypeEnum.MEMBER)
-  @UseGuards(AuthGuard(), RoleGuard)
-  async findOne(@Param('id', ParseIntPipe) id: number): Promise<GetUserDto> {
-    return await this.userService.get(id);
-  }
-
-  @Patch(':id')
+  @Patch()
   async update(
-    @Param('id', ParseIntPipe) id: string,
-    @Body() updateUserDto: UpdateUserDto,
+    @Headers('authorization') authHeader: string,
+    @Body() updateUserDto: Partial<User>,
   ): Promise<User> {
-    return this.userService.update(+id, updateUserDto);
+    return this.userService.update(updateUserDto, authHeader);
   }
 
-  @Delete(':id')
-  async remove(@Param('id', ParseIntPipe) id: number) {
-    return this.userService.delete(id);
+  @Delete()
+  async remove(@Headers('authorization') authHeader: string,) {
+    return this.userService.delete(authHeader);
+  }
+
+  @Patch("password")
+  async updatePasword(
+    @Headers('authorization') authHeader: string,
+    @Body() passwordDto
+  ): Promise<User> {
+    return this.userService.updatePassword(passwordDto, authHeader);
   }
 }
