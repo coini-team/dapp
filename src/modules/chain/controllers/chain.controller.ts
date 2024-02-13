4d21a4f14069c9b919e6c5afaa9afd42cfdc41a8
TincoNomad <renzotincopa@icloud.com>
2024-02-13 17:11:17 -0500

    ➕add: get chain & get network

--- src/modules/chain/controllers/chain.controller.ts
+++ src/modules/chain/controllers/chain.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ChainService } from '../services/chain.service';
+import { Chain } from '../entities/chain.entity';
+import { Network } from '../entities/network.entity';
 
 @Controller('chain')
 export class ChainController {
-  constructor(private readonly chainService: ChainService) {}
+  constructor(private readonly chainService: ChainService) { }
+
+  /**
+  * @memberof ChainController
+  * @description Find all Chain Endpoint.
+  * @returns {Promise<Chain[]>}
+  */
+  @Get()
+  get(): Promise<Chain[]> {
+    return this.chainService.get();
+  }
+
+  /**
+  * @memberof ChainController
+  * @description Find all Chain Endpoint.
+  * @param {string} id
+  * @returns {Promise<Chain[]>}
+  */
+  @Get(':id')
+  getNetworkById(
+    @Param('id') id: string,
+  ): Promise<Network[]> {
+    return this.chainService.getNetworkById(id);
+  }
 }
